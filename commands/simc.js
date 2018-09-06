29832afb408494d5a2ebdfe6030f315f9db01165
Liuyi Sun <sly.thu@gmail.com>
2018-09-06 02:32:42 -0400

    simc addon exports

--- commands/simc.js
+++ commands/simc.js
@@ -5,44 +5,97 @@ function generateResponse(name, profileImage, response) {
   let lines = response.data.split('\n');
   const dpsRankingLine = 'DPS Ranking:';
   let dpsLine = '';
-  for (let i = 0 ; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     let line = lines[i];
     console.log(`line[${i}] = ${lines[i]}`);
-    if (line == dpsRankingLine) {
+    if (line === dpsRankingLine) {
       dpsLine = lines[i + 1];
       console.log(`DPS result for ${name} is ${dpsLine}`);
       break;
     }
   }
-
   return new Discord.RichEmbed().setTitle('DPS simulation result for ' + name).
-      setAuthor(name , profileImage)
-      /*
-       * Alternatively, use '#00AE86', [0, 174, 134] or an integer number.
-       */.
+      setAuthor(name, profileImage).
       setColor(0x00AE86).
-      setDescription(
-          'Your quick simulation result. Click on the link to see the details.')
-      /*
-       * Takes a Date object, defaults to current date.
-       */.
-      setTimestamp().
       setURL(`https://liuyi.us/simc/results/${name}.html`).
       addField('Your DPS:', dpsLine);
 }
 
+const sendMessage = async (message, content) => {
+  console.log('Sending message..');
+  const msg = await message.channel.send(content);
+  console.log('Sending message complete!');
+  return msg;
+};
+
+const getProfile = async (name) => {
+  console.log('Getting profile for ' + name);
+  const profile = await axios.get(
+      `https://us.api.battle.net/wow/character/illidan/${name}?fields=appearance&locale=en_US&apikey=gg38sq3xg73j6h8qsjn4qu456zccx3qj`);
+  console.log('Getting profile complete');
+  return profile;
+};
+
+const simulate = async (name, server, realm) => {
+  console.log(`Simulating ${name} ${server} ${realm}`);
+  const simulationResult = await axios.get(
+      `https://liuyi.us/simc/?id=${name}&server=${server}&realm=${realm}`);
+  console.log('Simulation complete');
+  return simulationResult;
+};
+
+const simulateAddonExports = async (name, body) => {
+  console.log('Simulating addon exports ' + name);
+  const simulationResult = await axios.post(
+      'https://liuyi.us/simc/simcAddon.php',
+      'body=' + encodeURIComponent(body) + '&name=' + name);
+  console.log('Simulation complete');
+  return simulationResult;
+};
+
+const getName = (args) => {
+  if (args.length === 1) {
+    return args[0];
+  }
+  let simcExportedLines = args.join(' ').split('\n');
+  console.log(simcExportedLines);
+  for (let i = 0; i < simcExportedLines.length; i++) {
+    let line = simcExportedLines[i];
+    console.log('parsing: ' + line);
+    //rogue="Aierbeijuary"
+    const characterRegex = /^(deathknight|demonhunter|druid|hunter|mage|monk|paladin|priest|rogue|shaman|warrior|warlock)="?([a-zA-Z]+)"?$/;
+    if (characterRegex.test(line)) {
+      return characterRegex.exec(line)[2];
+    }
+  }
+  return null;
+};
+
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  console.log('simc args: ', args);
-  const name = args[0];
-  const profileResponse = await axios.get(`https://us.api.battle.net/wow/character/illidan/${name}?fields=appearance&locale=en_US&apikey=gg38sq3xg73j6h8qsjn4qu456zccx3qj`)
-  const profileImage = `https://render-us.worldofwarcraft.com/character/${profileResponse.data['thumbnail']}`;
-  console.log('profile:', profileImage);
+  console.log('Simulating args: ', args.length, args);
+  const name = getName(args);
+  let simcPromise = null;
+  if (name === null) {
+    return await sendMessage(message, `Hmm.. who do you want to simulate?`);
+  } else if (args.join(' ').length > 100) {
+    simcPromise = simulateAddonExports(name, args.join(' '));
+  } else {
+    simcPromise = simulate(name);
+  }
+  const messagePromise = sendMessage(message,
+      `Simulating ${name}, please be patient`);
+  const profilePromise = getProfile(name);
 
-  const httpResponse = await axios.get(`https://liuyi.us/simc/?id=${args[0]}`);
-  const response = generateResponse(args[0],profileImage, httpResponse);
-
-  const msg = await message.channel.send({embed: response});
-  //msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  try {
+    let [newMessage, profile, simcResult] = await Promise.all(
+        [messagePromise, profilePromise, simcPromise]);
+    const profileImage = `https://render-us.worldofwarcraft.com/character/${profile.data['thumbnail']}`;
+    const newMessageBody = generateResponse(name, profileImage, simcResult);
+    newMessage.edit({embed: newMessageBody});
+  } catch (e) {
+    console.log('Exception!');
+    sendMessage(message, 'Something\'s wrong... maybe ping const@illidan');
+  }
 };
 
 exports.conf = {
